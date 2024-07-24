f3f1ae0d85f461bb90856789fd74c7ca6197b690
Arun Chouhan <108116902+arunchouhan07@users.noreply.github.com>
2024-07-24 16:21:21 +0530

    Delete UserService directory

--- UserService/src/main/java/com/micro/userservice/exception/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.micro.userservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException() {super("Resource not found on the server");}
-    public ResourceNotFoundException(String message) {super(message);}
-}
--- UserService/src/main/java/com/micro/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.micro.userservice.repository;
-
-import com.micro.userservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
